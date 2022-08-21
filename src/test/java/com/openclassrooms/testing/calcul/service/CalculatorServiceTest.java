89a4404ffbed8556c7855ca5e051b4cb602b1882
bintou10 <98845023+bintou10@users.noreply.github.com>
2022-08-21 13:01:47 +0000

    Update CalculatorServiceTest.java

--- src/test/java/com/openclassrooms/testing/calcul/service/CalculatorServiceTest.java
+++ src/test/java/com/openclassrooms/testing/calcul/service/CalculatorServiceTest.java
@@ -114,7 +114,7 @@ public class CalculatorServiceTest {
 	@Test
 	public void calculate_shouldThrowIllegalArgumentAxception_forADivisionBy0() {
 		// GIVEN
-		when(calculator.divide(1, 0)).thenThrow(new ArithmeticException());
+		when(calculator.divide(2, 0)).thenThrow(new ArithmeticException());
 
 		// WHEN
 		assertThrows(IllegalArgumentException.class, () -> classUnderTest.calculate(
